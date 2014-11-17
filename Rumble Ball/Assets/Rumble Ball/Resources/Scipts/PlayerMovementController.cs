fd0767d86f71f130e38696583885f0d1bcc9cb84
Bruce Rick <brucerickti@gmail.com>
2014-11-17 01:44:34 -0500

    Added Rumble Ball networking prototype
    - Added models for stadium and player
    - Added physics to player, stadium and ball
    - Added networking
    - Created exe

---
+++ Rumble Ball/Assets/Rumble Ball/Resources/Scipts/PlayerMovementController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+	public float accelerationForce = 10f;
+	public float rotationForce = 3f;
+
+	void Awake () {
+		Debug.Log ("PLAYER AWAKE");
+	}
+
+	void Update () {
+
+		float rotation = Input.GetAxis("Horizontal");
+		float acceleration = -Input.GetAxis("Vertical");
+		
+		rigidbody.AddTorque(0, rotation * rotationForce, 0);
+		rigidbody.AddForce(transform.forward * acceleration * accelerationForce);
+
+	}
+}
